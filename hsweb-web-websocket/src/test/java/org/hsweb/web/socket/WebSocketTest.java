5b66d3568cf6424017dde555f4330ee5d6ccda5b
zh.sqy <zh.sqy@qq.com>
2016-01-19 23:39:31 +0800

    完善注释，和测试步骤

--- hsweb-web-websocket/src/test/java/org/hsweb/web/socket/WebSocketTest.java
+++ hsweb-web-websocket/src/test/java/org/hsweb/web/socket/WebSocketTest.java
@@ -35,9 +35,10 @@ public class WebSocketTest extends WebSocketConfigurationSupport {
      * 测试步骤
      * 1.运行成功后 请使用浏览器打开: http://localhost:8080
      * 2.忽略错误提示，按F12，进入console。
-     * 3.在Console 输入:var ws = new WebSocket('ws://localhost:8080/socket'); 按回车
-     * 4.再Console 输入：ws.send('{"cmd":"test"}'); 按回车
-     * 5.如果看到后台日志显示：handleMessage,id:0 msg={"cmd":"test"}，则代表成功了
+     * 3.执行:var ws = new WebSocket('ws://localhost:8080/socket');
+     * 3.执行:ws.onmessage=function(message){console.log(message.data)}
+     * 4.执行:ws.send('{"cmd":"test"}'); 按回车
+     * 5.如果看到后台日志显示：handleMessage,id:0 msg={"cmd":"test"}，前台有接收到推送消息，则代表成功了
      *
      * @throws Exception
      */
