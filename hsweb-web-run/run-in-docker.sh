38f312a7719621049df8cf1b4408230f2a19be44
周浩 <zh.sqy@qq.com>
2016-04-09 23:53:40 +0800

    优化逻辑

--- hsweb-web-run/run-in-docker.sh
+++ hsweb-web-run/run-in-docker.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 #mvn clean package -Pprod
+container_name=hsweb-web-run
+image_name=hsweb/web-run
+link_oracle=oracle11g
+server_port=9888
 if [ -f "target/hsweb-web-run-1.0-SNAPSHOT.jar" ]; then
         container_id=$(docker ps | grep "hsweb/web-run" | awk '{print $1}')
         if [ "container_id" != "" ];then
-            docker stop hsweb/web-run
-            docker rmi  hsweb/web-run
+            docker stop ${container_name}
+            docker rmi  ${image_name}
         fi
-            docker build -t hsweb/web-run .
-            docker run -d --link oracle11g:oracle -p 9888:8088 -p 5005:5005 --name hsweb hsweb/web-run
-           # docker run -it --rm --link oracle11gxe:oracle -p 80:8088 -p 5005:5005 --name hsweb hsweb/web-run
+            docker build -t ${image_name} .
+            docker run -d --link ${link_oracle}:oracle -p ${server_port}:8088 -p 5005:5005 --name ${container_name} ${image_name}
+           # docker run -it --rm --link oracle11gxe:oracle -p ${server_port}:8088 -p 5005:5005 --name ${container_name} ${image_name}
     else
         echo "build error!"
         exit -1
