28b53677a4914235066cea875d80f61349e61493
周浩 <zh.sqy@qq.com>
2016-04-09 23:04:27 +0800

    修改配置

--- hsweb-web-run/run-in-docker.sh
+++ hsweb-web-run/run-in-docker.sh
@@ -7,7 +7,7 @@ if [ -f "target/hsweb-web-run-1.0-SNAPSHOT.jar" ]; then
             docker rmi  hsweb/web-run
         fi
             docker build -t hsweb/web-run .
-            docker run -d --link oracle11gxe:oracle -p 80:8088 -p 5005:5005 --name hsweb hsweb/web-run
+            docker run -d --link oracle11g:oracle -p 80:8088 -p 5005:5005 --name hsweb hsweb/web-run
            # docker run -it --rm --link oracle11gxe:oracle -p 80:8088 -p 5005:5005 --name hsweb hsweb/web-run
     else
         echo "build error!"
