6353b3fdfd5021285d3738a8051e6313f44806a1
周浩 <zh.sqy@qq.com>
2016-04-09 22:19:26 +0800

    新增docker 部署配置

--- hsweb-web-run/run-in-docker.sh
+++ hsweb-web-run/run-in-docker.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 mvn clean package -Pprod
-docker build -t hsweb:test .
-docker run -it --rm --link oracle11gxe:oracle -p 80:8088 hsweb:test
+if [ -f "target/hsweb-web-run-1.0-SNAPSHOT.jar" ]; then
+        container_id=$(docker ps | grep "hsweb-web-run" | awk '{print $1}')
+        if [ "container_id" != "" ];then
+            docker stop hsweb/web-run
+            docker rmi  hsweb/web-run
+        fi
+            docker build -t hsweb/web-run .
+            docker run -d --link oracle11gxe:oracle -p 80:8088 -p 5005:5005 --name hsweb hsweb/web-run
+           # docker run -it --rm --link oracle11gxe:oracle -p 80:8088 -p 5005:5005 --name hsweb hsweb/web-run
+    else
+        echo "build error!"
+        exit -1
+fi
--- hsweb-web-run/src/main/java/org/hsweb/web/boot/Run.java
+++ hsweb-web-run/src/main/java/org/hsweb/web/boot/Run.java
@@ -5,9 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.servlet.support.RequestContext;
 
 /**
  * Created by 浩 on 2016-01-16 0016.
