e42205c0a23ef13d541f2e9a2272ed5304aa203c
周浩 <zh.sqy@qq.com>
2016-04-20 12:13:38 +0800

    丰富查询方式

--- hsweb-web-dao-interface/src/main/java/org/hsweb/web/dao/form/FormMapper.java
+++ hsweb-web-dao-interface/src/main/java/org/hsweb/web/dao/form/FormMapper.java
@@ -1,12 +1,40 @@
 package org.hsweb.web.dao.form;
 
+import org.hsweb.web.bean.common.QueryParam;
 import org.hsweb.web.dao.GenericMapper;
 import org.hsweb.web.bean.po.form.Form;
 
+import java.util.List;
+
 /**
-* 自定义表单数据映射接口
-* Created by generator 
-*/
-public interface FormMapper extends GenericMapper<Form,String> {
+ * 自定义表单数据映射接口
+ * Created by generator
+ */
+public interface FormMapper extends GenericMapper<Form, String> {
+
+    /**
+     * 查看当前正在使用的表单
+     *
+     * @param name 表单名字
+     * @return 表单对象
+     * @throws Exception
+     */
+    Form selectUsing(String name) throws Exception;
+
+    /**
+     * 查询最新版本的表单列表
+     *
+     * @param param 查询参数
+     * @return 表单列表
+     */
+    List<Form> selectLatestList(QueryParam param) throws Exception;
+
+    /**
+     * 查询最新版本的表单数量
+     *
+     * @param param 查询参数
+     * @return 表单数量
+     */
+    int countLatestList(QueryParam param) throws Exception;
 
 }
