72daae9e693d639a1edcaef262f8bc87ec0f6f02
周浩 <zh.sqy@qq.com>
2016-04-21 10:24:21 +0800

    新增数据库管理服务接口

---
+++ hsweb-web-service-interface/src/main/java/org/hsweb/web/service/system/DataBaseManagerService.java
@@ -0,0 +1,35 @@
+package org.hsweb.web.service.system;
+
+import org.hsweb.web.bean.common.database.TableField;
+
+import java.util.List;
+
+/**
+ * 数据库管理服务,用于获取数据库中的表结构等操作
+ * Created by zhouhao on 16-4-21.
+ */
+public interface DataBaseManagerService {
+
+    /**
+     * 获取当前数据源中所有的表名
+     *
+     * @return 表名集合
+     */
+    List<String> getTableNameList();
+
+    /**
+     * 获取数据库表的字段信息
+     *
+     * @param tableName 数据库表名
+     * @return 字段集合
+     */
+    List<TableField> getFieldList(String tableName);
+
+    /**
+     * 执行sql语句，多条sql语句使用[;\n]分割
+     *
+     * @param sql     sql语句
+     * @param process 执行过程回调，每执行一个sql都应该调用对应的回调方法
+     */
+    void executeSQL(String sql, SqlExecuteProcess process) throws Exception;
+}
