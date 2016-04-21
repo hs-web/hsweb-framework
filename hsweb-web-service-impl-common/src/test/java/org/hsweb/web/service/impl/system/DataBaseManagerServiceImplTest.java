0f207da4afa90afecee5794a510ade7e06bf9f2c
周浩 <zh.sqy@qq.com>
2016-04-21 10:33:27 +0800

    新增数据库管理服务单元测试

---
+++ hsweb-web-service-impl-common/src/test/java/org/hsweb/web/service/impl/system/DataBaseManagerServiceImplTest.java
@@ -0,0 +1,33 @@
+package org.hsweb.web.service.impl.system;
+
+import org.hsweb.web.service.impl.AbstractTestCase;
+import org.hsweb.web.service.system.DataBaseManagerService;
+import org.junit.Test;
+
+import javax.annotation.Resource;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zhouhao on 16-4-21.
+ */
+public class DataBaseManagerServiceImplTest extends AbstractTestCase {
+
+    @Resource
+    private DataBaseManagerService dataBaseManagerService;
+
+    @Test
+    public void testGetTableNameList() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFieldList() throws Exception {
+
+    }
+
+    @Test
+    public void testExecuteSQL() throws Exception {
+
+    }
+}
