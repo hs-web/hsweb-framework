cb4b008a279a6b442eb9fdec3dec56f05c955e05
周浩 <zh.sqy@qq.com>
2016-04-21 10:33:07 +0800

    新增数据库管理服务类

--- hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/system/DataBaseManagerServiceImpl.java
+++ hsweb-web-service-impl-common/src/main/java/org/hsweb/web/service/impl/system/DataBaseManagerServiceImpl.java
@@ -4,18 +4,24 @@ import org.hsweb.web.bean.common.database.TableField;
 import org.hsweb.web.service.system.DataBaseManagerService;
 import org.hsweb.web.service.system.SqlExecuteProcess;
 import org.springframework.stereotype.Service;
+import org.webbuilder.sql.support.executor.SqlExecutor;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 /**
  * 数据库管理服务实现类
  * Created by zhouhao on 16-4-21.
+ *
  * @author zhouhao,
  * @version 1.0
  * @see DataBaseManagerService
  */
 @Service(value = "dataBaseManagerService")
-public class DataBaseManangerServiceImpl implements DataBaseManagerService {
+public class DataBaseManagerServiceImpl implements DataBaseManagerService {
+    @Resource
+    private SqlExecutor sqlExecutor;
+
     @Override
     public List<String> getTableNameList() {
         return null;
