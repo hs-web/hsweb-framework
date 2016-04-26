d136e059792a8e53d6ecf41523150b3a728fd60e
周浩 <zh.sqy@qq.com>
2016-04-26 16:54:26 +0800

    新增自定义异常处理

--- hsweb-web-core/src/main/java/org/hsweb/web/exception/ExceptionHolderConfig.java
+++ hsweb-web-core/src/main/java/org/hsweb/web/exception/ExceptionHolderConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.request.RequestAttributes;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
