d77955d1c0367d233431f3d94ab4cdd0c22633f6
周浩 <zh.sqy@qq.com>
2016-04-27 17:09:56 +0800

    新增锁模块

---
+++ hsweb-web-concurrent/hsweb-web-concurrent-lock/src/main/java/org/hsweb/concurrent/lock/ReadWriteLockFactoryAutoConfig.java
@@ -0,0 +1,20 @@
+package org.hsweb.concurrent.lock;
+
+import org.hsweb.concurrent.lock.support.DefaultReadWriteLockFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zhouhao on 16-4-27.
+ */
+@Configuration
+@ConditionalOnMissingBean(value = {LockFactory.class})
+public class ReadWriteLockFactoryAutoConfig {
+
+    @Bean
+    public LockFactory createReadWriteLockFactory() {
+        return new DefaultReadWriteLockFactory();
+    }
+}
