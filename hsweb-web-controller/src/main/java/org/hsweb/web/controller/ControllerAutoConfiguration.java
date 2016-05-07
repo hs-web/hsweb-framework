157ec7e7dc44b1747d93ff0eed4fe85b828b6c91
周浩 <zh.sqy@qq.com>
2016-05-07 11:40:12 +0800

    新增自动配置

---
+++ hsweb-web-controller/src/main/java/org/hsweb/web/controller/ControllerAutoConfiguration.java
@@ -0,0 +1,15 @@
+package org.hsweb.web.controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by zhouhao on 16-5-6.
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {"org.hsweb.web.controller"}
+)
+public class ControllerAutoConfiguration {
+
+}
