f07d59d3687c149ac4066ddb99bedca831a2bc49
周浩 <zh.sqy@qq.com>
2016-05-13 14:22:04 +0800

    新增锁注解

---
+++ hsweb-web-concurrent/hsweb-web-concurrent-lock/src/main/java/org/hsweb/concurrent/lock/annotation/ReadLock.java
@@ -0,0 +1,20 @@
+package org.hsweb.concurrent.lock.annotation;
+
+import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by zhouhao on 16-5-13.
+ */
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ReadLock {
+    String value() default "";
+
+    String condition() default "";
+
+    long waitTime() default Long.MAX_VALUE;
+
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
+}
