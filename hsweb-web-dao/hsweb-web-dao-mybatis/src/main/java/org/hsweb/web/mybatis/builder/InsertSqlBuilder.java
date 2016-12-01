f09277194c012ac39f6f5780bc9e01a8efebfbff
zhouhao <zh.sqy@qq.com>
2016-12-01 11:11:16 +0800

    修复 跨表字段问题

--- hsweb-web-dao/hsweb-web-dao-mybatis/src/main/java/org/hsweb/web/mybatis/builder/InsertSqlBuilder.java
+++ hsweb-web-dao/hsweb-web-dao-mybatis/src/main/java/org/hsweb/web/mybatis/builder/InsertSqlBuilder.java
@@ -1,6 +1,9 @@
 package org.hsweb.web.mybatis.builder;
 
+import org.hsweb.ezorm.core.param.InsertParam;
+import org.hsweb.ezorm.rdb.executor.SQL;
 import org.hsweb.ezorm.rdb.meta.RDBColumnMetaData;
+import org.hsweb.ezorm.rdb.meta.RDBTableMetaData;
 import org.hsweb.ezorm.rdb.render.SqlAppender;
 import org.hsweb.ezorm.rdb.render.support.simple.SimpleInsertSqlRender;
 
@@ -8,6 +11,17 @@ import org.hsweb.ezorm.rdb.render.support.simple.SimpleInsertSqlRender;
  * @author zhouhao
  */
 public class InsertSqlBuilder extends SimpleInsertSqlRender {
+    @Override
+    public SQL render(RDBTableMetaData metaData, InsertParam param) {
+        RDBTableMetaData metaDataNew = metaData.clone();
+        metaDataNew.setDatabaseMetaData(metaData.getDatabaseMetaData());
+        metaDataNew.getColumns().stream()
+                .filter(column -> column.getName().contains("."))
+                .map(RDBColumnMetaData::getName)
+                .forEach(metaDataNew::removeColumn);
+        return super.render(metaDataNew, param);
+    }
+
     @Override
     protected SqlAppender getParamString(String paramName, RDBColumnMetaData rdbColumnMetaData) {
         return new SqlAppender().add("#{", paramName,
