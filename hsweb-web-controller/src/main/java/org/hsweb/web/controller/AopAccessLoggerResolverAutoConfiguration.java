b395fb679d7807010faf82be0fcbc8e54502d6f9
zhouhao <zh.sqy@qq.com>
2016-12-03 10:53:43 +0800

    新增Slf4jAccessLoggerPersisting

--- hsweb-web-controller/src/main/java/org/hsweb/web/controller/AopAccessLoggerResolverAutoConfiguration.java
+++ hsweb-web-controller/src/main/java/org/hsweb/web/controller/AopAccessLoggerResolverAutoConfiguration.java
@@ -8,6 +8,7 @@ import org.hsweb.web.bean.po.logger.LoggerInfo;
 import org.hsweb.web.bean.po.user.User;
 import org.hsweb.web.core.exception.BusinessException;
 import org.hsweb.web.core.logger.AccessLoggerPersisting;
+import org.hsweb.web.core.logger.Slf4jAccessLoggerPersisting;
 import org.hsweb.web.core.message.FastJsonHttpMessageConverter;
 import org.hsweb.web.core.message.ResponseMessage;
 import org.hsweb.web.core.utils.WebUtil;
@@ -27,6 +28,11 @@ public class AopAccessLoggerResolverAutoConfiguration {
     @Bean
     public AopAccessLoggerResolverConfiguration aopAccessLoggerResolverConfiguration() {
         return new AopAccessLoggerResolverConfiguration();
+    }
+
+    @Bean
+    public Slf4jAccessLoggerPersisting slf4jAccessLoggerPersisting() {
+        return new Slf4jAccessLoggerPersisting();
     }
 
     @Aspect
