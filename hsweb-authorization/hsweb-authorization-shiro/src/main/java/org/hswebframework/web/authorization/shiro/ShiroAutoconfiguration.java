4124897ec567c0510c30719a607c7db31e38740b
zhouhao <zh.sqy@qq.com>
2017-02-24 18:18:48 +0800

    优化aop拦截规则

--- hsweb-authorization/hsweb-authorization-shiro/src/main/java/org/hswebframework/web/authorization/shiro/ShiroAutoconfiguration.java
+++ hsweb-authorization/hsweb-authorization-shiro/src/main/java/org/hswebframework/web/authorization/shiro/ShiroAutoconfiguration.java
@@ -182,10 +182,16 @@ public class ShiroAutoconfiguration {
     @Aspect
     @Order(Ordered.HIGHEST_PRECEDENCE)
     static class MethodInterceptorHolderAdvisor {
-        @Around(value = "@annotation(org.hswebframework.web.authorization.annotation.RequiresExpression)" +
-                "||@annotation(org.hswebframework.web.authorization.annotation.RequiresDataAccess)" +
-                "||@annotation(org.hswebframework.web.authorization.annotation.Authorize)" +
-                "||within(@org.hswebframework.web.authorization.annotation.Authorize *)")
+        @Around(value = "@annotation(org.hswebframework.web.authorization.annotation.RequiresExpression)"
+                + "||@annotation(org.hswebframework.web.authorization.annotation.RequiresDataAccess)"
+                + "||@annotation(org.hswebframework.web.authorization.annotation.Authorize)"
+                + "||("
+                + "within(@org.hswebframework.web.authorization.annotation.Authorize *) "
+                + "&& ("
+                + "@annotation(org.springframework.web.bind.annotation.RequestMapping)||"
+                + "execution(org.hswebframework.web.controller.message.ResponseMessage *(..)"
+                + ")))"
+        )
         public Object around(ProceedingJoinPoint pjp) throws Throwable {
             MethodSignature signature = (MethodSignature) pjp.getSignature();
             String methodName = AopUtils.getMethodBody(pjp);
