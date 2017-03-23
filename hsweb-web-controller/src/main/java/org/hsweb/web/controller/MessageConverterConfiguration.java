e1e2c7ac3afb8745ff3ee7dcd901f26d14dbfd38
zhouhao <zh.sqy@qq.com>
2017-03-23 11:55:14 +0800

    2.2.1

--- hsweb-web-controller/src/main/java/org/hsweb/web/controller/MessageConverterConfiguration.java
+++ hsweb-web-controller/src/main/java/org/hsweb/web/controller/MessageConverterConfiguration.java
@@ -1,7 +1,9 @@
 package org.hsweb.web.controller;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import org.hsweb.web.bean.po.user.User;
 import org.hsweb.web.core.message.FastJsonHttpMessageConverter;
+import org.hsweb.web.core.message.ResponseMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -16,11 +18,20 @@ public class MessageConverterConfiguration {
     public HttpMessageConverter<Object> converter() {
         FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
         converter.setFeatures(
-        SerializerFeature.WriteNullListAsEmpty,
-        SerializerFeature.WriteNullNumberAsZero,
-        SerializerFeature.WriteNullBooleanAsFalse,
-        SerializerFeature.WriteDateUseDateFormat
-       );
-            return converter;
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteNullBooleanAsFalse,
+                SerializerFeature.WriteDateUseDateFormat
+        );
+        return converter;
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        user.setPassword("test");
+        user.setUsername("test1");
+        ResponseMessage message = ResponseMessage.created(user)
+                .exclude("password");
+        System.out.println(new FastJsonHttpMessageConverter().converter(message));
     }
 }
