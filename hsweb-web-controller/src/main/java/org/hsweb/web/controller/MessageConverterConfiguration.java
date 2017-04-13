16ea84a09cca1abe976e922c19ea8e5f7071365b
zhouhao <zh.sqy@qq.com>
2017-04-13 17:25:04 +0800

    删除无用代码

--- hsweb-web-controller/src/main/java/org/hsweb/web/controller/MessageConverterConfiguration.java
+++ hsweb-web-controller/src/main/java/org/hsweb/web/controller/MessageConverterConfiguration.java
@@ -1,9 +1,7 @@
 package org.hsweb.web.controller;
 
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import org.hsweb.web.bean.po.user.User;
 import org.hsweb.web.core.message.FastJsonHttpMessageConverter;
-import org.hsweb.web.core.message.ResponseMessage;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -24,14 +22,5 @@ public class MessageConverterConfiguration {
                 SerializerFeature.WriteDateUseDateFormat
         );
         return converter;
-    }
-
-    public static void main(String[] args) {
-        User user = new User();
-        user.setPassword("test");
-        user.setUsername("test1");
-        ResponseMessage message = ResponseMessage.created(user)
-                .exclude("password");
-        System.out.println(new FastJsonHttpMessageConverter().converter(message));
     }
 }
