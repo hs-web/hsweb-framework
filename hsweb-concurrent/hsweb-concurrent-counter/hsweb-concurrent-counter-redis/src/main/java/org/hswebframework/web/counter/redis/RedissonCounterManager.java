15d88768ff1c3c17efa9fde1a9802aeb64ec6109
zhouhao <zh.sqy@qq.com>
2017-05-11 18:27:04 +0800

    新增redis counter实现

---
+++ hsweb-concurrent/hsweb-concurrent-counter/hsweb-concurrent-counter-redis/src/main/java/org/hswebframework/web/counter/redis/RedissonCounterManager.java
@@ -0,0 +1,28 @@
+package org.hswebframework.web.counter.redis;
+
+import org.hswebframework.web.concurrent.counter.AbstractCounterManager;
+import org.hswebframework.web.concurrent.counter.Counter;
+import org.hswebframework.web.concurrent.counter.CounterManager;
+import org.redisson.Redisson;
+import org.redisson.api.RAtomicLong;
+
+import java.util.Map;
+
+/**
+ * @author zhouhao
+ */
+public class RedissonCounterManager extends AbstractCounterManager {
+
+    private Redisson redisson;
+
+    public RedissonCounterManager(Redisson redisson) {
+        this.redisson = redisson;
+    }
+
+    @Override
+    protected Counter createCount(String name, long initValue) {
+        Counter counter = new RedissonCounter(redisson.getAtomicLong(name));
+        counter.set(initValue);
+        return counter;
+    }
+}
