913c4db426255197db969f556e8ef4555fa63c31
zhouhao <zh.sqy@qq.com>
2017-06-28 14:34:14 +0800

    修改ip获取方式

--- hsweb-commons/hsweb-commons-utils/src/main/java/org/hswebframework/web/WebUtil.java
+++ hsweb-commons/hsweb-commons-utils/src/main/java/org/hswebframework/web/WebUtil.java
@@ -77,6 +77,9 @@ public class WebUtil {
     public static String getIpAddr(HttpServletRequest request) {
         String ip = request.getHeader(" x-forwarded-for ");
         if (ip == null || ip.length() == 0 || " unknown ".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("X-Forwarded-For");
+        }
+        if (ip == null || ip.length() == 0 || " unknown ".equalsIgnoreCase(ip)) {
             ip = request.getHeader("X-Real-IP");
         }
         if (ip == null || ip.length() == 0 || " unknown ".equalsIgnoreCase(ip)) {
