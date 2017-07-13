1123442a273a0765d65041c09f9b5fc8aea82dfa
zhouhao <admin@hsweb.me>
2017-07-13 22:36:41 +0800

    优化测试

--- hsweb-system/hsweb-system-dynamic-form/hsweb-system-dynamic-form-starter/src/test/java/org/hswebframework/web/service/form/simple/SimpleDynamicFormServiceTest.java
+++ hsweb-system/hsweb-system-dynamic-form/hsweb-system-dynamic-form-starter/src/test/java/org/hswebframework/web/service/form/simple/SimpleDynamicFormServiceTest.java
@@ -1,10 +1,10 @@
 package org.hswebframework.web.service.form.simple;
 
-import org.hsweb.ezorm.core.Trigger;
 import org.hsweb.ezorm.rdb.executor.SqlExecutor;
 import org.hswebframework.web.commons.entity.param.QueryParamEntity;
 import org.hswebframework.web.entity.form.DynamicFormColumnEntity;
 import org.hswebframework.web.entity.form.DynamicFormEntity;
+import org.hswebframework.web.service.form.DatabaseRepository;
 import org.hswebframework.web.service.form.DynamicFormColumnService;
 import org.hswebframework.web.service.form.DynamicFormOperationService;
 import org.hswebframework.web.service.form.DynamicFormService;
@@ -12,6 +12,7 @@ import org.hswebframework.web.tests.SimpleWebApplicationTests;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.JDBCType;
 import java.sql.SQLException;
@@ -34,19 +35,26 @@ public class SimpleDynamicFormServiceTest extends SimpleWebApplicationTests {
 
     @Autowired
     private DynamicFormOperationService dynamicFormOperationService;
+
+    @Autowired
+    private DatabaseRepository databaseRepository;
     @Autowired
     private SqlExecutor                 sqlExecutor;
 
     @Test
+    @Transactional
     public void testDeploy() throws SQLException {
         DynamicFormEntity form = entityFactory.newInstance(DynamicFormEntity.class);
         form.setName("test");
         form.setDatabaseTableName("f_test");
         form.setTriggers("[" +
-                "{\"trigger\":\"select.wrapper.done\"" +
+                "{\"trigger\":\"select.wrapper.done\"" +//触发器 在每个查询结果被包装为对象时触发
                 ",\"language\":\"groovy\"" +
                 ",\"script\":\"println('wrapper done:'+instance);return true;\"" +
                 "}" +
+                "]");
+        form.setCorrelations("[" +
+                "{\"target\":\"s_dyn_form\",\"alias\":\"form\",\"condition\":\"form.u_id=f_test.id\"}" +
                 "]");
         String id = dynamicFormService.insert(form);
         DynamicFormColumnEntity column_id = entityFactory.newInstance(DynamicFormColumnEntity.class);
@@ -78,15 +86,19 @@ public class SimpleDynamicFormServiceTest extends SimpleWebApplicationTests {
 
         dynamicFormOperationService.insert(form.getId(), new HashMap<String, Object>() {
             {
-                put("id", "test");
+                put("id", id);
                 put("name", "张三");
                 put("age", 10);
             }
         });
-        List<Object> objects = dynamicFormOperationService.select(form.getId(), new QueryParamEntity());
+
+        databaseRepository.getDefaultDatabase().getTable("s_dyn_form");
+
+        List<Object> objects = dynamicFormOperationService.select(form.getId(), new QueryParamEntity().includes("*","form.*"));
 
         Assert.assertTrue(objects.size() == 1);
         System.out.println(objects);
-        sqlExecutor.list("select * from f_test");
+        System.out.println(dynamicFormService.select());
+        System.out.println(sqlExecutor.list("select * from s_dyn_form"));
     }
 }
