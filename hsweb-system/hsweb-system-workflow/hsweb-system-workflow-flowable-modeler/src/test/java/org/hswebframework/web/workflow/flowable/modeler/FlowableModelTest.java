380f17240a2fd9620f90bb421593857945607e6a
zhouhao <admin@hsweb.me>
2017-07-20 22:33:49 +0800

    增加flowable工作流

---
+++ hsweb-system/hsweb-system-workflow/hsweb-system-workflow-flowable-modeler/src/test/java/org/hswebframework/web/workflow/flowable/modeler/FlowableModelTest.java
@@ -0,0 +1,18 @@
+package org.hswebframework.web.workflow.flowable.modeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by zhouhao on 2017/7/20.
+ */
+@SpringBootApplication
+public class FlowableModelTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowableModelTest.class,args);
+    }
+
+}
