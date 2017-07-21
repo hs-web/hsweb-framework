38d64fead087cebc822c1cb930ca2eb48c43bd7d
zhouhao <zh.sqy@qq.com>
2017-07-21 16:20:30 +0800

    新增模板模块

---
+++ hsweb-system/hsweb-system-template/hsweb-system-template-dao/hsweb-system-template-dao-api/src/main/java/org/hswebframework/web/dao/tempalte/TemplateDao.java
@@ -0,0 +1,11 @@
+package org.hswebframework.web.dao.tempalte;
+
+import org.hswebframework.web.dao.CrudDao;
+import org.hswebframework.web.entity.tempalte.TemplateEntity;
+
+/**
+*  模板 DAO接口
+*  @author hsweb-generator-online
+ */
+public interface TemplateDao extends CrudDao<TemplateEntity,String> {
+}
---
+++ hsweb-system/hsweb-system-template/hsweb-system-template-service/hsweb-system-template-service-api/src/main/java/org/hswebframework/web/service/tempalte/TemplateService.java
@@ -0,0 +1,13 @@
+package org.hswebframework.web.service.tempalte;
+
+import org.hswebframework.web.entity.tempalte.TemplateEntity;
+import org.hswebframework.web.service.CrudService;
+
+/**
+ * 模板 服务类
+ *
+ * @author hsweb-generator-online
+ */
+public interface TemplateService extends CrudService<TemplateEntity, String> {
+
+}
