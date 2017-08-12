1074979030dcff5ae9f924967b3629ff87dfd1b6
zhouhao <admin@hsweb.me>
2017-08-12 09:12:42 +0800

    优化权限，整合在一起

--- hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/annotation/Authorize.java
+++ hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/annotation/Authorize.java
@@ -36,7 +36,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
-public @interface Authorize {
+public @interface  Authorize {
 
     /**
      * 对角色授权,当使用按角色授权时，对模块以及操作级别授权方式失效
@@ -95,4 +95,10 @@ public @interface Authorize {
      * @return 是否忽略, 忽略后将不进行权限控制
      */
     boolean ignore() default false;
+
+    /**
+     *
+     * @return 数据权限控制
+     */
+    RequiresDataAccess[] dataAccess()default {};
 }
