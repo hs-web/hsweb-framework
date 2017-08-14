7c2508576a997d562c8fcbca91268498b660b114
zhouhao <zh.sqy@qq.com>
2017-08-14 18:25:55 +0800

    修复漏洞

--- hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/HswebAutoConfiguration.java
+++ hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/HswebAutoConfiguration.java
@@ -105,6 +105,8 @@ public class HswebAutoConfiguration {
                     if (classType.isEnum()) {
                         return super.getDeserializer(type);
                     }
+                    checkAutoType(type.getTypeName(), ((Class) type));
+
                     if (Modifier.isAbstract(classType.getModifiers()) || Modifier.isInterface(classType.getModifiers())) {
                         if (entityFactory != null && (Entity.class.isAssignableFrom(classType) || Model.class.isAssignableFrom(classType))) {
                             return new JavaBeanDeserializer(this, entityFactory.getInstanceType(classType), type);
@@ -118,9 +120,8 @@ public class HswebAutoConfiguration {
         };
 
         //fastjson.parser.autoTypeAccept
-        ParserConfig.getGlobalInstance().addAccept("org.hswebframework.web.entity.");
-        ParserConfig.getGlobalInstance().addAccept("org.hsweb.");
-        ParserConfig.getGlobalInstance().addDeny("org.hsweb.ezorm.core.param.SqlTerm");
+        ParserConfig.global.addAccept("org.hswebframework.web.entity.");
+        ParserConfig.global.addDeny("org.hsweb.ezorm.core.param.SqlTerm");
         return converter;
     }
 
