6f6f9bbbfc04b44ae250ba1e17df413e8253fdf8
zhouhao <zh.sqy@qq.com>
2017-08-16 14:56:15 +0800

    增加无参构造方法

--- hsweb-core/src/main/java/org/hswebframework/web/NotFoundException.java
+++ hsweb-core/src/main/java/org/hswebframework/web/NotFoundException.java
@@ -25,4 +25,8 @@ public class NotFoundException extends BusinessException {
     public NotFoundException(String message) {
         super(message, 404);
     }
+
+    public NotFoundException() {
+        this("data not found");
+    }
 }
