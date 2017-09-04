2995dc7e2f248bbfe9459d4b6c0c264d780ca413
zhouhao <zh.sqy@qq.com>
2017-09-04 17:00:21 +0800

    优化响应实体

---
+++ hsweb-system/hsweb-system-workflow/hsweb-system-workflow-flowable/src/test/java/org/hswebframework/web/workflow/flowable/TestApplication.java
@@ -0,0 +1,17 @@
+package org.hswebframework.web.workflow.flowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * TODO 完成注释
+ *
+ * @author zhouhao
+ */
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+    }
+}
