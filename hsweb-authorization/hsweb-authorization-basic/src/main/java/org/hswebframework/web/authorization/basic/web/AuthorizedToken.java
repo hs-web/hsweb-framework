dfc6dd756a27ec03de6376d1fee1d91b49c5b4a9
zhouhao <zh.sqy@qq.com>
2017-09-13 14:40:59 +0800

    优化注释

--- hsweb-authorization/hsweb-authorization-basic/src/main/java/org/hswebframework/web/authorization/basic/web/AuthorizedToken.java
+++ hsweb-authorization/hsweb-authorization-basic/src/main/java/org/hswebframework/web/authorization/basic/web/AuthorizedToken.java
@@ -1,8 +1,7 @@
 package org.hswebframework.web.authorization.basic.web;
 
 /**
- * TODO 完成注释
- *
+ * 已完成认证的令牌,如果返回此令牌,将直接使用{@link this#getUserId()}来绑定用户信息
  * @author zhouhao
  */
 public interface AuthorizedToken extends ParsedToken {
