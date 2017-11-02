721b5db069c227dd4cc3e53d846570a07a702588
zhou-hao <i@hsweb.me>
2017-11-02 09:50:27 +0800

    优化日志逻辑

--- hsweb-logging/hsweb-access-logging-aop/src/main/java/org/hswebframework/web/loggin/aop/DefaultAccessLoggerParser.java
+++ hsweb-logging/hsweb-access-logging-aop/src/main/java/org/hswebframework/web/loggin/aop/DefaultAccessLoggerParser.java
@@ -5,6 +5,7 @@ import org.hswebframework.web.AopUtils;
 import org.hswebframework.web.boost.aop.context.MethodInterceptorHolder;
 import org.hswebframework.web.logging.AccessLogger;
 import org.hswebframework.web.logging.LoggerDefine;
+import org.springframework.core.annotation.AnnotationUtils;
 
 import java.lang.reflect.Method;
 import java.util.Objects;
@@ -14,7 +15,7 @@ import java.util.stream.Stream;
 public class DefaultAccessLoggerParser implements AccessLoggerParser {
     @Override
     public boolean support(Class clazz, Method method) {
-        AccessLogger ann = AopUtils.findAnnotation(clazz, method, AccessLogger.class);
+        AccessLogger ann = AnnotationUtils.findAnnotation(method, AccessLogger.class);
         //注解了并且未取消
         return null != ann && !ann.ignore();
     }
