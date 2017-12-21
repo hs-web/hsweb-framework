96bc62dd5a428cf82656c1351055aa9d3f960737
zhouhao <zh.sqy@qq.com>
2017-12-21 13:50:43 +0800

    优化test

--- hsweb-concurrent/hsweb-concurrent-lock/hsweb-concurrent-lock-starter/src/test/java/org/hswebframework/web/concurrent/lock/starter/LockAnnotationTest.java
+++ hsweb-concurrent/hsweb-concurrent-lock/hsweb-concurrent-lock-starter/src/test/java/org/hswebframework/web/concurrent/lock/starter/LockAnnotationTest.java
@@ -4,20 +4,25 @@ import org.hswebframework.web.concurrent.lock.LockManager;
 import org.hswebframework.web.tests.SimpleWebApplicationTests;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * TODO 完成注释
  *
  * @author zhouhao
  */
-@Configuration
-public class LockAnnotationTest extends SimpleWebApplicationTests {
-
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = TestApplication.class)
+public class LockAnnotationTest extends AbstractTransactionalJUnit4SpringContextTests {
     @Autowired
     private LockService lockService;
 
@@ -27,15 +32,9 @@ public class LockAnnotationTest extends SimpleWebApplicationTests {
     @Test
     public void testLock() throws InterruptedException {
         new Thread(() -> {
-            try {
-                System.out.println("锁住");
-                lockManager.getLock("lock_test").lock();
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            System.out.println("锁住");
+            lockService.testLockSleep("test", 2000);
             System.out.println("解锁");
-            lockManager.getLock("lock_test").unlock();
         }).start();
         Thread.sleep(200);
         System.out.println("开始任务1");
--- hsweb-concurrent/hsweb-concurrent-lock/hsweb-concurrent-lock-starter/src/test/java/org/hswebframework/web/concurrent/lock/starter/LockService.java
+++ hsweb-concurrent/hsweb-concurrent-lock/hsweb-concurrent-lock-starter/src/test/java/org/hswebframework/web/concurrent/lock/starter/LockService.java
@@ -1,19 +1,37 @@
 package org.hswebframework.web.concurrent.lock.starter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.hswebframework.web.concurrent.lock.annotation.Lock;
 import org.hswebframework.web.concurrent.lock.annotation.ReadLock;
 import org.hswebframework.web.concurrent.lock.annotation.WriteLock;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 /**
- * TODO 完成注释
- *
  * @author zhouhao
  */
-@Service
+@Slf4j
 public class LockService {
 
     private long counter = 0;
+
+//    @Scheduled(cron = "0/1 * * * * ?")
+//    @Lock("test2")
+//    public void test() throws InterruptedException {
+//        log.info("try lock");
+//        Thread.sleep(5000);
+//        log.info("un lock");
+//    }
+
+    @Lock("lock_${#key}")
+    public long testLockSleep(String key, long time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return counter;
+    }
 
     @Lock("lock_${#key}")
     public long testLock(String key) {
