6b631e142902245f76f3294e62e8e995b13b2994
zhou-hao <i@hsweb.me>
2018-01-31 21:32:57 +0800

    add SQLException handle

--- hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/RestControllerExceptionTranslator.java
+++ hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/RestControllerExceptionTranslator.java
@@ -36,6 +36,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -116,7 +117,15 @@ public class RestControllerExceptionTranslator {
     @ResponseBody
     ResponseMessage handleException(RuntimeException exception) {
         logger.error(exception.getMessage(), exception);
-        return ResponseMessage.error(400, exception.getMessage());
+        return ResponseMessage.error(500, exception.getMessage());
+    }
+
+    @ExceptionHandler(SQLException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    ResponseMessage handleException(SQLException exception) {
+        logger.error(exception.getMessage(), exception);
+        return ResponseMessage.error(500,"服务器内部错误");
     }
 
 
