7e1a4c894f4f3e9f1eadecfdfd5627d02ccf8d2c
zhouhao <zh.sqy@qq.com>
2018-02-09 18:27:12 +0800

    优化转换

--- hsweb-system/hsweb-system-organizational/hsweb-system-organizational-authorization/src/main/java/org/hswebframework/web/organizational/authorization/simple/CustomScopeDataAccessConfigConvert.java
+++ hsweb-system/hsweb-system-organizational/hsweb-system-organizational-authorization/src/main/java/org/hswebframework/web/organizational/authorization/simple/CustomScopeDataAccessConfigConvert.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import org.hswebframework.web.authorization.access.DataAccessConfig;
 import org.hswebframework.web.authorization.simple.builder.DataAccessConfigConvert;
 import org.hswebframework.web.organizational.authorization.access.DataAccessType;
+import org.springframework.util.StringUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -25,6 +26,9 @@ public class CustomScopeDataAccessConfigConvert implements DataAccessConfigConve
 
     @Override
     public DataAccessConfig convert(String type, String action, String config) {
+        if (StringUtils.isEmpty(config)) {
+            config = "{}";
+        }
         SimpleCustomScopeDataAccessConfig accessConfig = JSON.parseObject(config, SimpleCustomScopeDataAccessConfig.class);
         accessConfig.setAction(action);
 
