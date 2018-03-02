e170451e22e43898d91d959fc19fa4881118f64e
zhouhao <zh.sqy@qq.com>
2018-03-02 18:02:26 +0800

    优化template

--- hsweb-system/hsweb-system-dynamic-form/hsweb-system-dynamic-form-service/hsweb-system-dynamic-form-service-simple/src/main/java/org/hswebframework/web/service/form/simple/SimpleDynamicFormColumnService.java
+++ hsweb-system/hsweb-system-dynamic-form/hsweb-system-dynamic-form-service/hsweb-system-dynamic-form-service-simple/src/main/java/org/hswebframework/web/service/form/simple/SimpleDynamicFormColumnService.java
@@ -18,10 +18,12 @@ public class SimpleDynamicFormColumnService extends GenericEntityService<Dynamic
         implements DynamicFormColumnService {
     @Autowired
     private DynamicFormColumnDao dynamicFormColumnDao;
-   @Override
+
+    @Override
     protected IDGenerator<String> getIDGenerator() {
         return IDGenerator.MD5;
     }
+
     @Override
     public DynamicFormColumnDao getDao() {
         return dynamicFormColumnDao;
