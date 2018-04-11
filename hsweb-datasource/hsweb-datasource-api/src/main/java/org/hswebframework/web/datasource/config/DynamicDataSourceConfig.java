8d872213d0ee329565cdbaf0d3087eabbffbd082
zhou-hao <i@hsweb.me>
2018-04-11 23:35:27 +0800

    序列化标识

--- hsweb-datasource/hsweb-datasource-api/src/main/java/org/hswebframework/web/datasource/config/DynamicDataSourceConfig.java
+++ hsweb-datasource/hsweb-datasource-api/src/main/java/org/hswebframework/web/datasource/config/DynamicDataSourceConfig.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 
 @Data
 public class DynamicDataSourceConfig implements Serializable {
+    private static final long serialVersionUID = 2776152081818934459L;
     private String id;
 
     private String name;
