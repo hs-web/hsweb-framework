2b79b8f0403cefc157214cd294ede61106b99034
zhouhao <zh.sqy@qq.com>
2018-05-08 18:15:07 +0800

    修复数据源加载错误

--- hsweb-system/hsweb-system-database-manager/hsweb-system-database-manager-simple/src/main/java/org/hswebframework/web/datasource/manager/simple/SimpleDatabaseManagerService.java
+++ hsweb-system/hsweb-system-database-manager/hsweb-system-database-manager-simple/src/main/java/org/hswebframework/web/datasource/manager/simple/SimpleDatabaseManagerService.java
@@ -133,20 +133,16 @@ public class SimpleDatabaseManagerService implements DatabaseManagerService, Met
 
     @Override
     public Map<ObjectMetadata.ObjectType, List<? extends ObjectMetadata>> getMetas() {
-        String dsId = DataSourceHolder.switcher().currentDataSourceId();
         return parserRepo
                 .computeIfAbsent(DataSourceHolder.currentDatabaseType(), t -> new HashMap<>())
                 .entrySet()
-                .parallelStream()
+                .stream()
                 .collect(Collectors.toMap(Map.Entry::getKey, entry -> {
                     try {
-                        DataSourceHolder.switcher().use(dsId);
                         return entry.getValue().parseAll();
                     } catch (SQLException e) {
                         log.error("parse meta {} error", entry.getKey(), e);
                         return new ArrayList<>();
-                    } finally {
-                        DataSourceHolder.switcher().reset();
                     }
                 }));
     }
