30ecea0a14cc22931e1fae2166f788fdcc5bdc2d
zhouhao <zh.sqy@qq.com>
2018-07-30 12:09:29 +0800

    修复path错误

--- hsweb-system/hsweb-system-dev-tools/src/main/java/org/hswebframework/web/dev/tools/writer/DefaultCodeWriter.java
+++ hsweb-system/hsweb-system-dev-tools/src/main/java/org/hswebframework/web/dev/tools/writer/DefaultCodeWriter.java
@@ -22,7 +22,7 @@ public class DefaultCodeWriter implements CodeWriter {
         file.mkdir();
         String type = code.getType();
 
-        String filePath = code.getFile();
+        String filePath = path + "/" + code.getFile();
         if ("dir".equals(type)) {
             code.getChildren()
                     .forEach(childrenCode -> writeCode(filePath, childrenCode));
