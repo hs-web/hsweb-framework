27e7260bd5f23b9ee3ad40d774f34b7a5b4609c5
zhou-hao <i@hsweb.me>
2018-10-09 23:24:22 +0800

    fix bug #89

--- hsweb-system/hsweb-system-authorization/hsweb-system-authorization-starter/src/test/groovy/org/hswebframework/web/authorization/starter/TestApplication.java
+++ hsweb-system/hsweb-system-authorization/hsweb-system-authorization-starter/src/test/groovy/org/hswebframework/web/authorization/starter/TestApplication.java
@@ -1,6 +1,9 @@
 package org.hswebframework.web.authorization.starter;
 
+import org.hswebframework.web.authorization.basic.web.AuthorizationController;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
@@ -9,6 +12,13 @@ import org.springframework.test.context.web.WebAppConfiguration;
  */
 @SpringBootApplication
 @WebAppConfiguration
+@Configuration
 public class TestApplication {
+
+    @Bean
+    public AuthorizationController authorizationController() {
+        return new AuthorizationController();
+    }
+
 
 }
