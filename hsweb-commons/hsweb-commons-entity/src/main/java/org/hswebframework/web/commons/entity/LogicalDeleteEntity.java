4f0c255c346d6ce0a91fdcf82dd000496f4ab11c
zhouhao <zh.sqy@qq.com>
2019-01-17 16:53:03 +0800

    删除无用代码

--- hsweb-commons/hsweb-commons-entity/src/main/java/org/hswebframework/web/commons/entity/LogicalDeleteEntity.java
+++ hsweb-commons/hsweb-commons-entity/src/main/java/org/hswebframework/web/commons/entity/LogicalDeleteEntity.java
@@ -8,9 +8,6 @@ package org.hswebframework.web.commons.entity;
  */
 public interface LogicalDeleteEntity {
 
-    String deleted      = "deleted";
-    String deleteTime   = "deleteTime";
-
     Boolean getDeleted();
 
     void setDeleted(boolean deleted);
@@ -18,8 +15,5 @@ public interface LogicalDeleteEntity {
     Long getDeleteTime();
 
     void setDeleteTime(Long deleteTime);
-
-    String getDeleteUserId();
-
 
 }
