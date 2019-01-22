39c6e169e422556f82905eb863eba46dda854ee7
zhouhao <zh.sqy@qq.com>
2019-01-22 15:42:18 +0800

    修复日期数据类型

--- hsweb-system/hsweb-system-datasource/hsweb-system-datasource-starter/src/main/resources/hsweb-starter.js
+++ hsweb-system/hsweb-system-datasource/hsweb-system-datasource-starter/src/main/resources/hsweb-starter.js
@@ -24,7 +24,7 @@ function install(context) {
         .addColumn().name("u_id").alias("id").comment("ID").jdbcType(java.sql.JDBCType.VARCHAR).length(32).primaryKey().commit()
         .addColumn().name("name").alias("name").comment("数据源名称").jdbcType(java.sql.JDBCType.VARCHAR).length(64).commit()
         .addColumn().name("enabled").alias("enabled").comment("是否启用").jdbcType(java.sql.JDBCType.DECIMAL).length(4, 0).commit()
-        .addColumn().name("create_date").alias("createDate").comment("创建日期").jdbcType(java.sql.JDBCType.DATE).commit()
+        .addColumn().name("create_date").alias("createDate").comment("创建日期").jdbcType(java.sql.JDBCType.TIMESTAMP).commit()
         .addColumn().name("properties").alias("properties").comment("数据源配置").jdbcType(java.sql.JDBCType.CLOB).commit()
         .addColumn().name("describe").alias("describe").comment("备注").jdbcType(java.sql.JDBCType.VARCHAR).length(512).commit()
         .comment("数据源配置").commit();
