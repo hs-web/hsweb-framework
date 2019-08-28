60e6d4246be0e53049ef461f8c6fd0b715d372ee
zhou-hao <admin@hsweb.me>
2019-08-28 18:57:16 +0800

    实现ScopeDataAccessConfig

--- hsweb-system/hsweb-system-organizational/hsweb-system-organizational-authorization/src/main/java/org/hswebframework/web/organizational/authorization/simple/ScopeByUserDataAccessConfig.java
+++ hsweb-system/hsweb-system-organizational/hsweb-system-organizational-authorization/src/main/java/org/hswebframework/web/organizational/authorization/simple/ScopeByUserDataAccessConfig.java
@@ -3,6 +3,7 @@ package org.hswebframework.web.organizational.authorization.simple;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hswebframework.web.authorization.access.ScopeDataAccessConfig;
 import org.hswebframework.web.authorization.simple.AbstractDataAccessConfig;
 
 import java.util.Set;
@@ -14,7 +15,7 @@ import java.util.Set;
 @Getter
 @Setter
 @ToString
-public class ScopeByUserDataAccessConfig extends AbstractDataAccessConfig {
+public class ScopeByUserDataAccessConfig extends AbstractDataAccessConfig implements ScopeDataAccessConfig {
 
     private static final long serialVersionUID = 6678003761927318688L;
 
@@ -39,6 +40,10 @@ public class ScopeByUserDataAccessConfig extends AbstractDataAccessConfig {
     @Override
     public boolean equals(Object obj) {
         return obj instanceof ScopeByUserDataAccessConfig && obj.hashCode() == hashCode();
+    }
+
+    public Set<Object> getScope(){
+        return ((Set) scope);
     }
 
 }
