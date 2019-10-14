8a4cf58ff7646ae03ba582069a6421b8ebbb3614
zhou-hao <admin@hsweb.me>
2019-10-14 14:50:12 +0800

    优化默认值生成器

--- hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/easyorm/generator/MD5Generator.java
+++ hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/easyorm/generator/MD5Generator.java
@@ -2,16 +2,17 @@ package org.hswebframework.web.starter.easyorm.generator;
 
 import org.hswebframework.ezorm.core.DefaultValueGenerator;
 import org.hswebframework.ezorm.core.RuntimeDefaultValue;
+import org.hswebframework.ezorm.rdb.metadata.RDBColumnMetadata;
 import org.hswebframework.web.id.IDGenerator;
 
-public class MD5Generator implements DefaultValueGenerator {
+public class MD5Generator implements DefaultValueGenerator<RDBColumnMetadata> {
     @Override
     public String getSortId() {
         return "md5";
     }
 
     @Override
-    public RuntimeDefaultValue generate() {
+    public RuntimeDefaultValue generate(RDBColumnMetadata metadata) {
         return IDGenerator.MD5::generate;
     }
 
--- hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/easyorm/generator/SnowFlakeStringIdGenerator.java
+++ hsweb-starter/hsweb-spring-boot-starter/src/main/java/org/hswebframework/web/starter/easyorm/generator/SnowFlakeStringIdGenerator.java
@@ -2,16 +2,17 @@ package org.hswebframework.web.starter.easyorm.generator;
 
 import org.hswebframework.ezorm.core.DefaultValueGenerator;
 import org.hswebframework.ezorm.core.RuntimeDefaultValue;
+import org.hswebframework.ezorm.rdb.metadata.RDBColumnMetadata;
 import org.hswebframework.web.id.IDGenerator;
 
-public class SnowFlakeStringIdGenerator implements DefaultValueGenerator {
+public class SnowFlakeStringIdGenerator implements DefaultValueGenerator<RDBColumnMetadata> {
     @Override
     public String getSortId() {
         return "snow_flake";
     }
 
     @Override
-    public RuntimeDefaultValue generate() {
+    public RuntimeDefaultValue generate(RDBColumnMetadata metadata) {
         return IDGenerator.SNOW_FLAKE_STRING::generate;
     }
 
