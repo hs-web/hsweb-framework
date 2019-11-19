c30f878f1f9d969db1cc6630fdc40d5c5f1e24dd
zhou-hao <admin@hsweb.me>
2019-11-19 18:26:34 +0800

    移除组织机构，先使用维度实现

--- hsweb-system/hsweb-system-organizational/hsweb-system-organizational-authorization/src/main/java/org/hswebframework/web/organizational/DepartmentAttachEntity.java
+++
@@ -1,9 +0,0 @@
-package org.hswebframework.web.organizational;
-
-public interface DepartmentAttachEntity {
-
-    String getDepartmentId();
-
-    void setDepartmentId(String positionId);
-
-}
--- hsweb-system/hsweb-system-organizational/hsweb-system-organizational-authorization/src/main/java/org/hswebframework/web/organizational/PositionAttachEntity.java
+++
@@ -1,9 +0,0 @@
-package org.hswebframework.web.organizational;
-
-public interface PositionAttachEntity {
-
-    String getPositionId();
-
-    void setPositionId(String positionId);
-
-}
