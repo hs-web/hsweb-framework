be1bf120f0e081a48c7c9ae8481488682ef65bfe
zhou-hao <admin@hsweb.me>
2020-08-19 15:19:27 +0800

    优化异常

--- hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/exception/AccessDenyException.java
+++ hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/exception/AccessDenyException.java
@@ -1,6 +1,8 @@
 package org.hswebframework.web.authorization.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
  * 权限验证异常
@@ -8,6 +10,7 @@ import lombok.Getter;
  * @author zhouhao
  * @since 3.0
  */
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class AccessDenyException extends RuntimeException {
 
     private static final long serialVersionUID = -5135300127303801430L;
