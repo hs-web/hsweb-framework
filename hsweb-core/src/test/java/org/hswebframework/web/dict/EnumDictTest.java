01bcd247bd18694f2f1022d0e031591eb816d16a
zhouhao <zh.sqy@qq.com>
2021-06-23 18:26:35 +0800

    优化枚举反序列化

--- hsweb-core/src/test/java/org/hswebframework/web/dict/EnumDictTest.java
+++ hsweb-core/src/test/java/org/hswebframework/web/dict/EnumDictTest.java
@@ -1,13 +1,18 @@
 package org.hswebframework.web.dict;
 
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.deser.std.EnumDeserializer;
+import com.fasterxml.jackson.databind.module.SimpleDeserializers;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.databind.type.ClassKey;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -18,27 +23,46 @@ public class EnumDictTest {
     public void testJackson() {
         ObjectMapper mapper = new ObjectMapper();
         SimpleModule module = new SimpleModule();
-        JsonDeserializer<EnumDict> deserialize = new EnumDict.EnumDictJSONDeserializer();
-        module.addDeserializer(Enum.class, (JsonDeserializer) deserialize);
+        module.setDeserializers(new SimpleDeserializers() {
+            @Override
+            public JsonDeserializer<?> findEnumDeserializer(Class<?> type,
+                                                            DeserializationConfig config,
+                                                            BeanDescription beanDesc) throws JsonMappingException {
+                JsonDeserializer<?> deser = null;
+                if (type.isEnum()) {
+                    if (EnumDict.class.isAssignableFrom(type)) {
+                        deser = new EnumDict.EnumDictJSONDeserializer(val -> EnumDict
+                                .find((Class) type, val)
+                                .orElse(null));
+                    }
+                }
+                return deser;
+            }
+        });
         mapper.registerModule(module);
 
 
-        String val = mapper.writer().writeValueAsString(new TestEntity());
+//        String val = mapper.writer().writeValueAsString(new TestEntity());
+//
+//        System.out.println(val);
+//        TestEntity testEntity = mapper.readerFor(TestEntity.class)
+//                                      .readValue(val);
+//
+//        Assert.assertEquals(testEntity.testEnum, TestEnum.E1);
+//        testEntity = mapper.readerFor(TestEntity.class)
+//                           .readValue("{\"testEnum\":\"E1\"}");
+//        Assert.assertEquals(testEntity.testEnum, TestEnum.E1);
+//
+//        testEntity = mapper.readerFor(TestEntity.class)
+//                           .readValue("{\"testEnum\":\"e1\"}");
+//        Assert.assertEquals(testEntity.testEnum, TestEnum.E1);
+//
+//        System.out.println((Object) mapper.readerFor(TestEnum.class).readValue("\"E1\""));
 
-        System.out.println(val);
         TestEntity testEntity = mapper.readerFor(TestEntity.class)
-                                      .readValue(val);
-
-        Assert.assertEquals(testEntity.testEnum, TestEnum.E1);
-        testEntity = mapper.readerFor(TestEntity.class)
-                           .readValue("{\"testEnum\":\"E1\"}");
-        Assert.assertEquals(testEntity.testEnum, TestEnum.E1);
-
-        testEntity = mapper.readerFor(TestEntity.class)
-                           .readValue("{\"testEnum\":\"e1\"}");
-        Assert.assertEquals(testEntity.testEnum, TestEnum.E1);
-
-        System.out.println((Object) mapper.readerFor(TestEnum.class).readValue("\"E1\""));
+                                      .readValue("{\"testEnums\":[\"E1\"]}");
+        System.out.println(testEntity.getTestEnums());
+//        Assert.assertArrayEquals(testEntity.getSimpleEnums(), new TestEnum[]{TestEnum.E1});
 
     }
 
@@ -49,8 +73,10 @@ public class EnumDictTest {
 
         assertTrue(EnumDict.find(TestEnum.class, "e1")
                            .isPresent());
+
         assertTrue(EnumDict.find(TestEnum.class, "E1")
                            .isPresent());
+
 
     }
 
@@ -60,6 +86,8 @@ public class EnumDictTest {
         private TestEnum testEnum = TestEnum.E1;
 
         private SimpleEnum simpleEnum = SimpleEnum.A;
+
+        private TestEnum[] testEnums;
     }
 
     public enum SimpleEnum {
