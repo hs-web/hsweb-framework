90f92870973d3ca24d844dd8360c3e2d8240a498
zhouhao <zh.sqy@qq.com>
2022-12-01 15:52:38 +0800

    优化ID生成策略

--- hsweb-core/src/main/java/org/hswebframework/web/id/RandomIdGenerator.java
+++ hsweb-core/src/main/java/org/hswebframework/web/id/RandomIdGenerator.java
@@ -1,11 +1,21 @@
 package org.hswebframework.web.id;
 
 import io.netty.util.concurrent.FastThreadLocal;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 
 import java.util.Base64;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class RandomIdGenerator {
+
+    // java -Dgenerator.random.instance-id=8
+    private static final RandomIdGenerator GLOBAL = new RandomIdGenerator(
+            Integer.getInteger("generator.random.instance-id", ThreadLocalRandom.current().nextInt()).byteValue()
+    );
 
     private final static FastThreadLocal<byte[]> HOLDER = new FastThreadLocal<byte[]>() {
         @Override
@@ -14,16 +24,59 @@ public class RandomIdGenerator {
         }
     };
 
+    private final byte instanceId;
+
+    public static RandomIdGenerator create(byte instanceId) {
+        return new RandomIdGenerator(instanceId);
+    }
 
     public static String random() {
-        return random(HOLDER.get());
+        return GLOBAL.generate();
     }
 
-    public static String random(byte[] container) {
+    public String generate() {
+        long now = System.currentTimeMillis();
+        byte[] value = HOLDER.get();
+        value[0] = instanceId;
 
-        ThreadLocalRandom.current().nextBytes(container);
+        value[1] = (byte) (now >>> 32);
+        value[2] = (byte) (now >>> 24);
+        value[3] = (byte) (now >>> 16);
+        value[4] = (byte) (now >>> 8);
+        value[5] = (byte) (now);
 
-        return Base64.getUrlEncoder().encodeToString(container);
+        nextBytes(value, 6, 8);
+        nextBytes(value, 9, 15);
+        nextBytes(value, 16, 24);
+
+        return Base64.getUrlEncoder().encodeToString(value);
     }
 
+
+    private static void nextBytes(byte[] bytes, int offset, int len) {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+
+        for (int i = offset; i < len; ) {
+            for (int rnd = random.nextInt(),
+                 n = Math.min(len - i, Integer.SIZE / Byte.SIZE);
+                 n-- > 0; rnd >>= Byte.SIZE) {
+                bytes[i++] = (byte) rnd;
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(random());
+        System.out.println(random().length());
+        Set<String> distinct = new HashSet<>(100_0000);
+
+        long time = System.currentTimeMillis();
+        for (int i = 0; i < 100_0000; i++) {
+            distinct.add(random());
+        }
+        System.out.println(distinct.size());
+        System.out.println(System.currentTimeMillis() - time);
+    }
 }
