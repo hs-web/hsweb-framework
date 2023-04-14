a5fd80f008ce73695cf000dd5d336b513aee8682
zhouhao <zh.sqy@qq.com>
2023-04-14 15:55:11 +0800

    fix: 替换弃用API

--- hsweb-starter/src/main/java/org/hswebframework/web/starter/jackson/CustomTypeFactory.java
+++ hsweb-starter/src/main/java/org/hswebframework/web/starter/jackson/CustomTypeFactory.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.type.*;
 import com.fasterxml.jackson.databind.util.ArrayBuilders;
 import com.fasterxml.jackson.databind.util.LRUMap;
+import com.fasterxml.jackson.databind.util.LookupCache;
 import org.hswebframework.web.api.crud.entity.EntityFactory;
 
 public class CustomTypeFactory extends TypeFactory {
@@ -15,7 +16,7 @@ public class CustomTypeFactory extends TypeFactory {
         this.entityFactory = factory;
     }
 
-    protected CustomTypeFactory(LRUMap<Object, JavaType> typeCache, TypeParser p,
+    protected CustomTypeFactory(LookupCache<Object, JavaType> typeCache, TypeParser p,
                                 TypeModifier[] mods, ClassLoader classLoader) {
         super(typeCache, p, mods, classLoader);
     }
@@ -33,7 +34,7 @@ public class CustomTypeFactory extends TypeFactory {
 
     @Override
     public TypeFactory withModifier(TypeModifier mod) {
-        LRUMap<Object, JavaType> typeCache = _typeCache;
+        LookupCache<Object, JavaType> typeCache = _typeCache;
         TypeModifier[] mods;
         if (mod == null) { // mostly for unit tests
             mods = null;
