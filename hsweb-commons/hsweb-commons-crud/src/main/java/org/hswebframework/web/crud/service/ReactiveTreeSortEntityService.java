d18dfa3cc5d8334236c316be8159e26597008cb0
zhouhao <zh.sqy@qq.com>
2023-07-12 13:33:40 +0800

    refactor: 优化树结构实体更新逻辑

--- hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/service/ReactiveTreeSortEntityService.java
+++ hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/service/ReactiveTreeSortEntityService.java
@@ -4,11 +4,14 @@ import org.hswebframework.ezorm.rdb.mapping.defaults.SaveResult;
 import org.hswebframework.ezorm.rdb.operator.dml.Terms;
 import org.hswebframework.utils.RandomUtil;
 import org.hswebframework.web.api.crud.entity.QueryParamEntity;
+import org.hswebframework.web.api.crud.entity.TransactionManagers;
 import org.hswebframework.web.api.crud.entity.TreeSortSupportEntity;
 import org.hswebframework.web.api.crud.entity.TreeSupportEntity;
 import org.hswebframework.web.id.IDGenerator;
 import org.hswebframework.web.validator.CreateGroup;
 import org.reactivestreams.Publisher;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -46,6 +49,7 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
      * @param paramEntity 查询参数
      * @return 树形结构
      */
+    @Transactional(readOnly = true, transactionManager = TransactionManagers.reactiveTransactionManager)
     default Mono<List<E>> queryResultToTree(QueryParamEntity paramEntity) {
         return query(paramEntity)
                 .collectList()
@@ -60,6 +64,7 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
      * @param paramEntity 查询参数
      * @return 树形结构
      */
+    @Transactional(readOnly = true, transactionManager = TransactionManagers.reactiveTransactionManager)
     default Mono<List<E>> queryIncludeChildrenTree(QueryParamEntity paramEntity) {
         return queryIncludeChildren(paramEntity)
                 .collectList()
@@ -74,12 +79,12 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
      * @param idList ID集合
      * @return 树形结构
      */
+    @Transactional(readOnly = true, transactionManager = TransactionManagers.reactiveTransactionManager)
     default Flux<E> queryIncludeChildren(Collection<K> idList) {
         Set<String> duplicateCheck = new HashSet<>();
 
         return findById(idList)
-                .concatMap(e -> StringUtils
-                        .isEmpty(e.getPath()) || !duplicateCheck.add(e.getPath())
+                .concatMap(e -> !StringUtils.hasText(e.getPath()) || !duplicateCheck.add(e.getPath())
                         ? Mono.just(e)
                         : createQuery()
                         .where()
@@ -95,12 +100,12 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
      * @param idList ID集合
      * @return 树形结构
      */
+    @Transactional(readOnly = true, transactionManager = TransactionManagers.reactiveTransactionManager)
     default Flux<E> queryIncludeParent(Collection<K> idList) {
         Set<String> duplicateCheck = new HashSet<>();
 
         return findById(idList)
-                .concatMap(e -> StringUtils
-                        .isEmpty(e.getPath()) || !duplicateCheck.add(e.getPath())
+                .concatMap(e -> !StringUtils.hasText(e.getPath()) || !duplicateCheck.add(e.getPath())
                         ? Mono.just(e)
                         : createQuery()
                         .where()
@@ -118,12 +123,12 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
      * @param queryParam 查询参数
      * @return 树形结构
      */
+    @Transactional(readOnly = true, transactionManager = TransactionManagers.reactiveTransactionManager)
     default Flux<E> queryIncludeChildren(QueryParamEntity queryParam) {
         Set<String> duplicateCheck = new HashSet<>();
 
         return query(queryParam)
-                .concatMap(e -> StringUtils
-                        .isEmpty(e.getPath()) || !duplicateCheck.add(e.getPath())
+                .concatMap(e -> !StringUtils.hasText(e.getPath()) || !duplicateCheck.add(e.getPath())
                         ? Mono.just(e)
                         : createQuery()
                         .where()
@@ -134,6 +139,7 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
     }
 
     @Override
+    @Transactional(readOnly = true, transactionManager = TransactionManagers.reactiveTransactionManager)
     default Mono<Integer> insert(Publisher<E> entityPublisher) {
         return insertBatch(Flux.from(entityPublisher).collectList());
     }
@@ -150,7 +156,7 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
 
     default Mono<E> applyTreeProperty(E ele) {
         if (StringUtils.hasText(ele.getPath()) ||
-                StringUtils.isEmpty(ele.getParentId())) {
+                ObjectUtils.isEmpty(ele.getParentId())) {
             return Mono.just(ele);
         }
 
@@ -162,7 +168,7 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
 
     //校验是否有循环依赖,修改父节点为自己的子节点?
     default Mono<E> checkCyclicDependency(K id, E ele) {
-        if (StringUtils.isEmpty(id)) {
+        if (ObjectUtils.isEmpty(id)) {
             return Mono.empty();
         }
         return this
@@ -182,7 +188,7 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
                 .where("parentId", id)
                 .fetch()
                 .flatMap(e -> {
-                    if (StringUtils.isEmpty(path)) {
+                    if (ObjectUtils.isEmpty(path)) {
                         e.setPath(RandomUtil.randomChar(4));
                     } else {
                         e.setPath(path + "-" + RandomUtil.randomChar(4));
@@ -318,23 +324,31 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
     }
 
     @Override
+    @Transactional(transactionManager = TransactionManagers.reactiveTransactionManager)
     default Mono<SaveResult> save(Collection<E> collection) {
         return save(Flux.fromIterable(collection));
     }
 
     @Override
+    @Transactional(transactionManager = TransactionManagers.reactiveTransactionManager)
     default Mono<SaveResult> save(E data) {
         return save(Flux.just(data));
     }
 
     @Override
+    @Transactional(transactionManager = TransactionManagers.reactiveTransactionManager)
     default Mono<Integer> updateById(K id, Mono<E> entityPublisher) {
         return this
-                .save(entityPublisher.doOnNext(e -> e.setId(id)))
-                .map(SaveResult::getTotal);
+                .findById(id)
+                .map(e -> this
+                        .save(entityPublisher.doOnNext(data -> data.setId(id)))
+                        .map(SaveResult::getTotal))
+                .defaultIfEmpty(Mono.just(0))
+                .flatMap(Function.identity());
     }
 
     @Override
+    @Transactional(transactionManager = TransactionManagers.reactiveTransactionManager)
     default Mono<Integer> deleteById(Publisher<K> idPublisher) {
         return this
                 .findById(Flux.from(idPublisher))
@@ -358,7 +372,7 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
                 return true;
             }
             //有父节点,但是父节点不存在
-            if (!StringUtils.isEmpty(node.getParentId())) {
+            if (!ObjectUtils.isEmpty(node.getParentId())) {
                 return helper.getNode(node.getParentId()) == null;
             }
             return false;
@@ -366,6 +380,6 @@ public interface ReactiveTreeSortEntityService<E extends TreeSortSupportEntity<K
     }
 
     default boolean isRootNode(E entity) {
-        return StringUtils.isEmpty(entity.getParentId()) || "-1".equals(String.valueOf(entity.getParentId()));
+        return ObjectUtils.isEmpty(entity.getParentId()) || "-1".equals(String.valueOf(entity.getParentId()));
     }
 }
