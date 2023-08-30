809655ab6b2cd4384191ee0e23bba0926fcdab16
zhangji <125540670@qq.com>
2023-08-30 17:04:14 +0800

    fix(枚举字典): 优化反序列化的空值判断

--- hsweb-core/src/main/java/org/hswebframework/web/dict/EnumDict.java
+++ hsweb-core/src/main/java/org/hswebframework/web/dict/EnumDict.java
@@ -170,7 +170,10 @@ public interface EnumDict<V> extends JSONSerializable {
      * @see EnumDict#find(Class, Predicate)
      */
     static <T extends Enum<?> & EnumDict<?>> Optional<T> findByValue(Class<T> type, Object value) {
-        return find(type, e -> e.getValue() == value || e.getValue().equals(value) || value != null && String
+        if (value == null) {
+            return Optional.empty();
+        }
+        return find(type, e -> e.getValue() == value || e.getValue().equals(value) || String
                 .valueOf(e.getValue())
                 .equalsIgnoreCase(String.valueOf(value)));
     }
