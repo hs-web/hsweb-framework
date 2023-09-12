3c494aa0cab4626d5c55ef42f9e43b27e818167b
老周 <zh.sqy@qq.com>
2023-09-12 16:58:25 +0800

    fix: 修复SpelSqlExpressionInvoker无法解析下划线命名的列. (#239)

--- hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/events/expr/AbstractSqlExpressionInvoker.java
+++ hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/events/expr/AbstractSqlExpressionInvoker.java
@@ -3,6 +3,7 @@ package org.hswebframework.web.crud.events.expr;
 import org.hswebframework.ezorm.rdb.mapping.EntityColumnMapping;
 import org.hswebframework.ezorm.rdb.operator.builder.fragments.NativeSql;
 import org.hswebframework.web.crud.events.SqlExpressionInvoker;
+import reactor.function.Function3;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,16 +11,16 @@ import java.util.function.BiFunction;
 
 public abstract class AbstractSqlExpressionInvoker implements SqlExpressionInvoker {
 
-    private final Map<String, BiFunction<Object[], Map<String, Object>, Object>> compiled =
+    private final Map<String, Function3<EntityColumnMapping,Object[], Map<String, Object>, Object>> compiled =
             new ConcurrentHashMap<>();
 
     @Override
     public Object invoke(NativeSql sql, EntityColumnMapping mapping, Map<String, Object> object) {
         return compiled.computeIfAbsent(sql.getSql(), this::compile)
-                       .apply(sql.getParameters(), object);
+                       .apply(mapping,sql.getParameters(), object);
     }
 
 
-    protected abstract BiFunction<Object[], Map<String, Object>, Object> compile(String sql);
+    protected abstract Function3<EntityColumnMapping,Object[], Map<String, Object>, Object> compile(String sql);
 
 }
