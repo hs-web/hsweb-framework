705a95c87ff045509e24685baabfdc25def0a583
liusq <106655480+liu4410@users.noreply.github.com>
2023-09-22 10:15:59 +0800

    fix(数据字典): 修复新增字典时未进行校验的问题 (#242)

--- hsweb-system/hsweb-system-dictionary/src/main/java/org/hswebframework/web/dictionary/entity/DictionaryEntity.java
+++ hsweb-system/hsweb-system-dictionary/src/main/java/org/hswebframework/web/dictionary/entity/DictionaryEntity.java
@@ -27,6 +27,7 @@ import org.hswebframework.web.api.crud.entity.RecordCreationEntity;
 import org.hswebframework.web.crud.generator.Generators;
 import org.hswebframework.web.dict.DictDefine;
 import org.hswebframework.web.dict.defaults.DefaultDictDefine;
+import org.hswebframework.web.validator.CreateGroup;
 
 import javax.persistence.Column;
 import javax.persistence.Table;
@@ -45,7 +46,7 @@ import java.util.List;
 public class DictionaryEntity extends GenericEntity<String> implements RecordCreationEntity {
     //字典名称
     @Column(nullable = false)
-    @NotBlank(message = "名称不能为空")
+    @NotBlank(message = "名称不能为空",groups = CreateGroup.class)
     @Schema(description = "字典名称")
     private String name;
     //分类
