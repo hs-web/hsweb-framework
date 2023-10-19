faf540af7bbb11133183e9c143f89546068392e9
gyl <1554863190@qq.com>
2023-10-19 15:55:04 +0800

    feat: 优化

--- hsweb-core/src/main/java/org/hswebframework/web/bean/Diff.java
+++ hsweb-core/src/main/java/org/hswebframework/web/bean/Diff.java
@@ -1,6 +1,7 @@
 package org.hswebframework.web.bean;
 
 import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,9 +21,10 @@ public class Diff {
 
     private Object after;
 
+
     public static List<Diff> of(Object before, Object after, String... ignoreProperty) {
         List<Diff> diffs = new ArrayList<>();
-        List<String> ignores = Arrays.asList(ignoreProperty);
+        Set<String> ignores = Sets.newHashSet(ignoreProperty);
 
         Map<String, Object> beforeMap = FastBeanCopier.copy(before, HashMap::new);
         Map<String, Object> afterMap = FastBeanCopier.copy(after, HashMap::new);
