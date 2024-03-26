56ad57dcb922ca97c3a63a8237bf88b5cb54f10b
gyl <1554863190@qq.com>
2024-03-26 17:08:40 +0800

    feat: 登录接口记录操作日志用户认证信息

--- hsweb-authorization/hsweb-authorization-basic/src/main/java/org/hswebframework/web/authorization/basic/web/AuthorizationController.java
+++ hsweb-authorization/hsweb-authorization-basic/src/main/java/org/hswebframework/web/authorization/basic/web/AuthorizationController.java
@@ -33,8 +33,10 @@ import org.hswebframework.web.authorization.exception.AuthenticationException;
 import org.hswebframework.web.authorization.exception.UnAuthorizedException;
 import org.hswebframework.web.authorization.simple.PlainTextUsernamePasswordAuthenticationRequest;
 import org.hswebframework.web.logging.AccessLogger;
+import org.hswebframework.web.logging.AccessLoggerInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
 import org.springframework.http.MediaType;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
@@ -119,6 +121,23 @@ public class AuthorizationController {
                                 .then(Mono.error(failedEvent::getException));
                     });
         });
+    }
+
+    @EventListener
+    public void fillLoggerInfoAuth(AuthorizationSuccessEvent event) {
+        event.async(
+                //填充操作日志用户认证信息
+                Mono.deferContextual(ctx -> {
+                    AccessLoggerInfo loggerInfo = ctx.get(AccessLoggerInfo.class);
+                    Authentication auth = event.getAuthentication();
+                    loggerInfo.putContext("userId", auth.getUser().getId());
+                    loggerInfo.putContext("username", auth.getUser().getUsername());
+                    loggerInfo.putContext("userName", auth.getUser().getName());
+                    // FIXME: 2024/3/26 未传递用户维度信息,如有需要也可通过上下文传递
+                    return Mono.empty();
+                })
+        );
+
     }
 
     private Mono<Authentication> doAuthorize(AuthorizationBeforeEvent event) {
