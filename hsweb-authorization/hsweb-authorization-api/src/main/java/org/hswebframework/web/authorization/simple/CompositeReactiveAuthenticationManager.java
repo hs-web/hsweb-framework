68de2eff7b5683c1eba0392a495f435cd6499f8e
gyl <1554863190@qq.com>
2024-04-15 16:45:03 +0800

    refactor: 优化获取权限错误传递

--- hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/simple/CompositeReactiveAuthenticationManager.java
+++ hsweb-authorization/hsweb-authorization-api/src/main/java/org/hswebframework/web/authorization/simple/CompositeReactiveAuthenticationManager.java
@@ -3,10 +3,15 @@ package org.hswebframework.web.authorization.simple;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
-import org.hswebframework.web.authorization.*;
+import org.hswebframework.web.authorization.Authentication;
+import org.hswebframework.web.authorization.AuthenticationRequest;
+import org.hswebframework.web.authorization.ReactiveAuthenticationManager;
+import org.hswebframework.web.authorization.ReactiveAuthenticationManagerProvider;
+import org.hswebframework.web.authorization.exception.AuthenticationException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -19,17 +24,27 @@ public class CompositeReactiveAuthenticationManager implements ReactiveAuthentic
 
     @Override
     public Mono<Authentication> authenticate(Mono<AuthenticationRequest> request) {
+        List<AuthenticationException> exception = new ArrayList<>();
         return Flux.concat(providers
                                    .stream()
                                    .map(manager -> manager
                                            .authenticate(request)
                                            .onErrorResume((err) -> {
                                                log.warn("get user authenticate error", err);
+                                               if (err instanceof AuthenticationException) {
+                                                   exception.add((AuthenticationException) err);
+                                               }
                                                return Mono.empty();
                                            }))
                                    .collect(Collectors.toList()))
                    .take(1)
-                   .next();
+                   .singleOrEmpty()
+                   .switchIfEmpty(Mono.defer(() -> {
+                       if (!exception.isEmpty()) {
+                           return Mono.error(() -> exception.get(0));
+                       }
+                       return Mono.empty();
+                   }));
     }
 
     @Override
