c5552c939649f1fea48fdc848e882c3607b3d12c
zhouhao <zh.sqy@qq.com>
2025-02-20 19:01:35 +0800

    refactor: 优化错误报告逻辑

--- hsweb-core/src/main/java/org/hswebframework/web/exception/analyzer/ExceptionAnalyzerReporter.java
+++ hsweb-core/src/main/java/org/hswebframework/web/exception/analyzer/ExceptionAnalyzerReporter.java
@@ -47,9 +47,7 @@ public class ExceptionAnalyzerReporter implements ExceptionAnalyzer {
 
     protected void addSimpleReporter(Pattern pattern, Consumer<Throwable> reporter) {
 
-        addReporter((error) -> error.getMessage() != null
-                        && pattern.matcher(error.getMessage()).matches(),
-                    reporter);
+        addReporter((error) -> pattern.matcher(error.toString()).matches(), reporter);
     }
 
     public boolean doReportException(Throwable failure) {
