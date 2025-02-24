a2c0da447b58db55570183a21dabb766d8580ce3
zhouhao <zh.sqy@qq.com>
2025-02-24 10:04:03 +0800

    refactor: 优化join支持

--- hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/query/JoinNestConditionalSpec.java
+++ hsweb-commons/hsweb-commons-crud/src/main/java/org/hswebframework/web/crud/query/JoinNestConditionalSpec.java
@@ -1,19 +1,16 @@
 package org.hswebframework.web.crud.query;
 
 import org.hswebframework.ezorm.core.NestConditional;
-import org.hswebframework.ezorm.core.StaticMethodReferenceColumn;
 import org.hswebframework.ezorm.core.TermTypeConditionalSupport;
-import org.hswebframework.ezorm.core.param.TermType;
-import org.hswebframework.ezorm.rdb.operator.builder.fragments.term.AbstractTermFragmentBuilder;
 
 public interface JoinNestConditionalSpec<C extends TermTypeConditionalSupport>
-        extends JoinOnSpec<C>, NestConditional<C> {
+        extends JoinOnSpec<JoinNestConditionalSpec<C>>, NestConditional<C> {
 
     @Override
-    JoinNestConditionalSpec<NestConditional<C>> nest();
+    JoinNestConditionalSpec<JoinNestConditionalSpec<C>> nest();
 
     @Override
-    JoinNestConditionalSpec<NestConditional<C>> orNest();
+    JoinNestConditionalSpec<JoinNestConditionalSpec<C>> orNest();
 
 
 }
