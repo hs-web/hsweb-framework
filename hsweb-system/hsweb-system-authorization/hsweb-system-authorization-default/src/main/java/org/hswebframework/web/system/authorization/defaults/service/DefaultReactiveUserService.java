21f14781489881368727366a8f327b530d323ebf
fighter-wang <118291973+fighter-wang@users.noreply.github.com>
2025-04-18 15:45:58 +0800

    fix(用户保存): 修复保存用户信息时，抛出的UserModifiedEvent事件中的最新用户信息不完整的问题 (#316)
    * fix(用户保存): 修复保存用户信息时抛出的UserModifiedEvent事件中的最新用户信息不完整的问题
    
    * fix(用户保存): 代码优化
    
    * fix(用户保存): 修复old实体会被修改的问题
    
    ---------
    
    Co-authored-by: fighter-wang <11291691+fighter-wang@user.noreply.gitee.com>

--- hsweb-system/hsweb-system-authorization/hsweb-system-authorization-default/src/main/java/org/hswebframework/web/system/authorization/defaults/service/DefaultReactiveUserService.java
+++ hsweb-system/hsweb-system-authorization/hsweb-system-authorization-default/src/main/java/org/hswebframework/web/system/authorization/defaults/service/DefaultReactiveUserService.java
@@ -119,13 +119,16 @@ public class DefaultReactiveUserService extends GenericReactiveCrudService<UserE
                         passwordValidator.validate(newer.getPassword());
                         newer.setPassword(passwordEncoder.encode(newer.getPassword(), newer.getSalt()));
                     }
+                    UserEntity copyEntity = old.copyTo(new UserEntity());
+                    UserEntity newEntity = newer.copyTo(copyEntity);
                     return getRepository()
                             .createUpdate()
                             .set(newer)
                             .where(newer::getId)
                             .execute()
-                            .flatMap(__ -> new UserModifiedEvent(old, newer, passwordChanged, newPassword).publish(eventPublisher))
-                            .thenReturn(newer)
+                            .flatMap(__ -> new UserModifiedEvent(old, newEntity, passwordChanged, newPassword)
+                                    .publish(eventPublisher)
+                                    .thenReturn(newEntity))
                             .flatMap(e -> ClearUserAuthorizationCacheEvent
                                     .of(e.getId())
                                     .publish(eventPublisher)
