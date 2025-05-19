dec90f8693be4b7c1b783becc6eb621eb9316dc7
zhangji <125540670@qq.com>
2025-05-19 10:26:23 +0800

    fix: 优化字典的并行执行

--- hsweb-system/hsweb-system-dictionary/src/main/java/org/hswebframework/web/dictionary/configuration/DictionaryProperties.java
+++ hsweb-system/hsweb-system-dictionary/src/main/java/org/hswebframework/web/dictionary/configuration/DictionaryProperties.java
@@ -15,6 +15,7 @@ import org.springframework.util.ClassUtils;
 
 import java.io.IOException;
 import java.util.*;
+import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 @ConfigurationProperties(prefix = "hsweb.dict")
@@ -34,36 +35,47 @@ public class DictionaryProperties {
         packages.add("org.hswebframework.web");
         CachingMetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory();
         ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
+        // 获取主线程的类加载器
+        Thread currentThread = Thread.currentThread();
+        Supplier<ClassLoader> classLoaderSupplier = currentThread::getContextClassLoader;
 
         return packages
-            .parallelStream()
-            .flatMap(enumPackage -> {
-                String path = "classpath*:" + ClassUtils.convertClassNameToResourcePath(enumPackage) + "/**/*.class";
-                log.info("scan enum dict package:{}", path);
-                Resource[] resources;
-                try {
-                    resources = resourcePatternResolver.getResources(path);
-                } catch (IOException e) {
-                    log.warn("scan enum dict package:{} error:", path, e);
-                    return Stream.empty();
-                }
-                return Stream
-                    .of(resources)
-                    .map(resource -> {
-                        try {
-                            MetadataReader reader = metadataReaderFactory.getMetadataReader(resource);
-                            String name = reader.getClassMetadata().getClassName();
-                            Class<?> clazz = ClassUtils.forName(name, null);
-                            if (clazz.isEnum() && EnumDict.class.isAssignableFrom(clazz)) {
-                                return clazz;
-                            }
-                        } catch (Throwable ignore) {
+                .parallelStream()
+                .flatMap(enumPackage -> {
+                    // 在每个任务中设置一致的类加载器
+                    ClassLoader prevClassLoader = Thread.currentThread().getContextClassLoader();
+                    Thread.currentThread().setContextClassLoader(classLoaderSupplier.get());
+                    String path = "classpath*:" + ClassUtils.convertClassNameToResourcePath(enumPackage) + "/**/*.class";
+                    log.info("scan enum dict package:{}", path);
+                    Resource[] resources;
+                    try {
+                        resources = resourcePatternResolver.getResources(path);
+                    } catch (IOException e) {
+                        log.warn("scan enum dict package:{} error:", path, e);
+                        return Stream.empty();
+                    }
+                    Stream<? extends Class<?>> stream = Stream
+                            .of(resources)
+                            .map(resource -> {
+                                try {
+                                    // 在每个任务中设置一致的类加载器
+                                    Thread.currentThread().setContextClassLoader(classLoaderSupplier.get());
+                                    MetadataReader reader = metadataReaderFactory.getMetadataReader(resource);
+                                    String name = reader.getClassMetadata().getClassName();
+                                    Class<?> clazz = ClassUtils.forName(name, classLoaderSupplier.get());
+                                    if (clazz.isEnum() && EnumDict.class.isAssignableFrom(clazz)) {
+                                        return clazz;
+                                    }
+                                } catch (Throwable ignore) {
 
-                        }
-                        return null;
-                    })
-                    .filter(Objects::nonNull);
-            });
+                                }
+                                return null;
+                            })
+                            .filter(Objects::nonNull);
+                    // 还原类加载器
+                    Thread.currentThread().setContextClassLoader(prevClassLoader);
+                    return stream;
+                });
 
     }
 }
