d27acdd489387a58283685d15e4741eae52e000c
zhangji <125540670@qq.com>
2025-05-19 14:53:54 +0800

    fix: 改为使用for循环实现

--- hsweb-system/hsweb-system-dictionary/src/main/java/org/hswebframework/web/dictionary/configuration/DictionaryProperties.java
+++ hsweb-system/hsweb-system-dictionary/src/main/java/org/hswebframework/web/dictionary/configuration/DictionaryProperties.java
@@ -15,7 +15,6 @@ import org.springframework.util.ClassUtils;
 
 import java.io.IOException;
 import java.util.*;
-import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 @ConfigurationProperties(prefix = "hsweb.dict")
@@ -35,49 +34,31 @@ public class DictionaryProperties {
         packages.add("org.hswebframework.web");
         CachingMetadataReaderFactory metadataReaderFactory = new CachingMetadataReaderFactory();
         ResourcePatternResolver resourcePatternResolver = new PathMatchingResourcePatternResolver();
-        // 获取主线程的类加载器
-        Thread currentThread = Thread.currentThread();
-        Supplier<ClassLoader> classLoaderSupplier = currentThread::getContextClassLoader;
 
-        return packages
-                .parallelStream()
-                .flatMap(enumPackage -> {
-                    ClassLoader prevClassLoader = Thread.currentThread().getContextClassLoader();
-                    try {
-                        // 在每个任务中设置一致的类加载器
-                        Thread.currentThread().setContextClassLoader(classLoaderSupplier.get());
-                        String path = "classpath*:" + ClassUtils.convertClassNameToResourcePath(enumPackage) + "/**/*.class";
-                        log.info("scan enum dict package:{}", path);
-                        Resource[] resources;
-                        try {
-                            resources = resourcePatternResolver.getResources(path);
-                        } catch (IOException e) {
-                            log.warn("scan enum dict package:{} error:", path, e);
-                            return Stream.empty();
-                        }
-                        return Stream
-                                .of(resources)
-                                .map(resource -> {
-                                    try {
-                                        // 在每个任务中设置一致的类加载器
-                                        Thread.currentThread().setContextClassLoader(classLoaderSupplier.get());
-                                        MetadataReader reader = metadataReaderFactory.getMetadataReader(resource);
-                                        String name = reader.getClassMetadata().getClassName();
-                                        Class<?> clazz = ClassUtils.forName(name, classLoaderSupplier.get());
-                                        if (clazz.isEnum() && EnumDict.class.isAssignableFrom(clazz)) {
-                                            return clazz;
-                                        }
-                                    } catch (Throwable ignore) {
-
-                                    }
-                                    return null;
-                                })
-                                .filter(Objects::nonNull);
-                    } finally {
-                        // 还原类加载器
-                        Thread.currentThread().setContextClassLoader(prevClassLoader);
+        List<Class<?>> classes = new ArrayList<>();
+        for (String enumPackage : packages) {
+            String path = "classpath*:" + ClassUtils.convertClassNameToResourcePath(enumPackage) + "/**/*.class";
+            log.info("scan enum dict package:{}", path);
+            Resource[] resources;
+            try {
+                resources = resourcePatternResolver.getResources(path);
+            } catch (IOException e) {
+                log.warn("scan enum dict package:{} error:", path, e);
+                return Stream.empty();
+            }
+            for (Resource resource : resources) {
+                try {
+                    MetadataReader reader = metadataReaderFactory.getMetadataReader(resource);
+                    String name = reader.getClassMetadata().getClassName();
+                    Class<?> clazz = ClassUtils.forName(name, null);
+                    if (clazz.isEnum() && EnumDict.class.isAssignableFrom(clazz)) {
+                        classes.add(clazz);
                     }
-                });
+                } catch (Throwable ignore) {
 
+                }
+            }
+        }
+        return classes.stream();
     }
 }
