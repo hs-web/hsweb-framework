2b492482065d108b3c7348061a66cfc7115e53da
zhouhao <zh.sqy@qq.com>
2025-07-11 10:57:11 +0800

    refactor: 优化权限初始化

--- changes.sh
+++ changes.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-git diff --name-only HEAD~1 HEAD | \
+# 收集变更模块
+modules=$(git diff --name-only HEAD~1 HEAD | \
 while read file; do
   dir=$(dirname "$file")
   while [ "$dir" != "." ] && [ "$dir" != "/" ]; do
     if [ -f "$dir/pom.xml" ]; then echo "$dir"; break; fi
     dir=$(dirname "$dir")
   done
-done | sort -u | tr '\n' ',' | sed 's/,$//'
+done | sort -u | tr '\n' ',' | sed 's/,$//')
 
 # 如果为空，则使用默认值 '.'
 if [ -z "$modules" ]; then
